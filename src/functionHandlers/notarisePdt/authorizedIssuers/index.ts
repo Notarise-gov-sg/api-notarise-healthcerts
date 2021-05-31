0bbb64d7828fc5d6aedc3d3754b03b525b9a53b1
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-05-31 20:18:19 +0800

    feat: refactor whitelist

--- src/functionHandlers/notarisePdt/authorizedIssuers/index.ts
+++ src/functionHandlers/notarisePdt/authorizedIssuers/index.ts
@@ -1,6 +1,29 @@
-import { authorizedIssuers } from "./authorizedIssuersMap";
+import axios from "axios";
+import urljoin from "url-join";
+import { getLogger } from "../../../common/logger";
+import { config } from "../../../config";
 
-export const isAuthorizedIssuer = (domain: string): boolean =>
-  authorizedIssuers.has(domain.toLowerCase());
+const { trace, error } = getLogger("src/functionHandlers/notarisePdt/authorizedIssuers/index");
 
-export const getIssuer = authorizedIssuers.get;
+export const isAuthorizedIssuer = async (domain: string): Promise<boolean> => {
+  try {
+    const headers = {
+      "x-api-key": `${config.authorizedIssuers.apiKey}`,
+      "Content-Type": "application/json",
+    };
+    const getAuthorizedIssuerUrl = urljoin(
+      `${config.authorizedIssuers.endpoint}`,
+      "api/v1/vaccinations",
+      domain.toLowerCase(),
+      "pcr"
+    );
+    const response = await axios.get(getAuthorizedIssuerUrl, {
+      headers,
+    });
+    trace(`Retrieve ${domain} whitelist : ${JSON.stringify(response.data)}`);
+    return Promise.resolve(response.status === 200);
+  } catch (e) {
+    error(`Unable to find ${domain} whitelist : ${JSON.stringify(e)}`);
+    return false;
+  }
+};
