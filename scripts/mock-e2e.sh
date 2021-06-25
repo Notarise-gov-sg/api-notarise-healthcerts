c70f1b69be2930379796fe64d6219d4e8a22df51
eugbyte <eugenetham1994@gmail.com>
2021-06-25 17:25:32 +0800

    chore: updated github action

---
+++ scripts/mock-e2e.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is just to use in development to test the e2e portion of the github action
+
+# if AUTHORIZED_ISSUERS_API_URL environment variable is not set, set it
+echo "STAGING_BASE_URL: <$STAGING_BASE_URL>"
+if [[ -z "$STAGING_BASE_URL" ]]
+    then 
+        STAGING_BASE_URL="http://localhost:4000/dev"
+fi
+
+echo "STAGING_BASE_URL: <$STAGING_BASE_URL>"
+
+# --fail returns an exit code > 0 when the request fails. -- fail sets output to silent, --show-error undoes it.
+# NEED TO ADD x-api-key
+curl -vX POST $STAGING_BASE_URL/notarise/pdt \
+-d @test/fixtures/example_healthcert_with_nric_wrapped.json \
+--fail --show-error \
+--header "Content-Type: application/json" \
+--header 'x-api-key: d41d8cd98f00b204e9800998ecf8427e' 
