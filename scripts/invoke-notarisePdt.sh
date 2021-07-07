6cdf687c601fbb1bbfc35dc376fd7a9e20814222
eugbyte <eugenetham1994@gmail.com>
2021-07-07 13:32:02 +0800

    fix: comment

--- scripts/invoke-notarisePdt.sh
+++ scripts/invoke-notarisePdt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DEV_URL="http://localhost:4000/dev"
-X_API_KEY="d41d8cd98f00b204e9800998ecf8427e"
+X_API_KEY="YOUR KEY"
 
 curl -vX POST "$DEV_URL/notarise/pdt" \
         -d @./test/fixtures/example_healthcert_with_nric_wrapped.json \
