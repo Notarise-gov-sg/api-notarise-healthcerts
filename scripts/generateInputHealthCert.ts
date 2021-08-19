2e4b82f60167c1394748c5e07b005178d5154a1b
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-08-19 13:26:43 +0800

    fix: lint

--- scripts/generateInputHealthCert.ts
+++ scripts/generateInputHealthCert.ts
@@ -7,8 +7,7 @@ import { writeFileSync } from "fs";
 import { config } from "../src/config";
 import sample from "../test/fixtures/v2/pdt_pcr_with_nric_unwrapped.json";
 
-const OUTPUT_FILE =
-  "test/fixtures/v2/pdt_pcr_with_nric_wrapped.json";
+const OUTPUT_FILE = "test/fixtures/v2/pdt_pcr_with_nric_wrapped.json";
 
 const run = async () => {
   const healthCert = sample as v2.OpenAttestationDocument;
