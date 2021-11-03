e8f1383033200be7d9d72053e534506a7b028eff
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-11-03 15:43:34 +0800

    Chore: 180045605 remove loading public/private keys value from ssm direct (#337)
    * Chore: 180045605 remove loading public/private keys value from ssm direct
    
    * fix: remove gpay async syntax
    
    * fix: remove gpay async syntax

--- src/models/euHealthCert/createEuHealthCert/createEuSignedTestQr.ts
+++ src/models/euHealthCert/createEuHealthCert/createEuSignedTestQr.ts
@@ -1,18 +1,26 @@
 import { signAndPack, makeCWT } from "@pathcheck/dcc-sdk";
 import { notarise } from "@govtechsg/oa-schemata";
 import { EuHealthCert } from "../../../types";
-import { config } from "../../../config";
+import { config, getDefaultIfUndefined } from "../../../config";
 
 const { euSigner } = config;
 
 export const createEuSignedTestQr = async (euHealthCerts: EuHealthCert[]) => {
+  const publicKey = getDefaultIfUndefined(
+    process.env.SIGNING_EU_QR_PUBLIC_KEY,
+    ""
+  ).replace(/\\n/g, "\n");
+  const privateKey = getDefaultIfUndefined(
+    process.env.SIGNING_EU_QR_PRIVATE_KEY,
+    ""
+  ).replace(/\\n/g, "\n");
   const testHealthCertsQr: notarise.SignedEuHealthCert[] = [];
   await Promise.all(
     euHealthCerts.map(async (euHealthCert) => {
       const qrData = await signAndPack(
         await makeCWT(euHealthCert, null, euSigner.name),
-        euSigner.publicKey,
-        euSigner.privateKey
+        publicKey,
+        privateKey
       );
       const testType =
         euHealthCert.t[0].tt === "LP6464-4"
