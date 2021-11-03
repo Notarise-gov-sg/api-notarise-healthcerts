31f8bdbd8e113d3c99b2155495f0b1d66f31ca4c
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-11-03 15:43:34 +0800

    Chore: 180045605 remove loading public/private keys value from ssm direct (#337)
    * Chore: 180045605 remove loading public/private keys value from ssm direct
    
    * fix: remove gpay async syntax
    
    * fix: remove gpay async syntax

--- src/functionHandlers/middyfy.ts
+++ src/functionHandlers/middyfy.ts
@@ -4,9 +4,11 @@ import jsonBodyParser from "@middy/http-json-body-parser";
 import httpErrorHandler from "@middy/http-error-handler";
 import { cloudWatchMiddleware } from "../middleware/cloudWatch";
 import { validateSchemaMiddleware } from "../middleware/validateSchema";
+import { withSsm } from "../middleware/withSsm";
 
 export const middyfy = (handler: Handler) =>
   middy(handler).use([
+    withSsm,
     jsonBodyParser(),
     validateSchemaMiddleware(),
     cloudWatchMiddleware(),
