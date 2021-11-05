564d22b9b1e0c21b602bd12cb0deac8752320ba0
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-11-05 13:24:09 +0800

    Chore: 180045605 remove loading public/private keys value from ssm direct (#342)
    * Chore: 180045605 remove loading public/private keys value from ssm direct (#337)
    
    * Chore: 180045605 remove loading public/private keys value from ssm direct
    
    * fix: remove gpay async syntax
    
    * fix: remove gpay async syntax
    
    * chore: add comment in ssm middleware

--- src/models/gpayCovidCard/index.ts
+++ src/models/gpayCovidCard/index.ts
@@ -5,14 +5,19 @@ import GPay, {
 } from "@notarise-gov-sg/gpay-covid-cards";
 import { Bundle } from "../../models/fhir/types";
 import { isoToDateOnlyString, isoToLocaleString } from "../../common/datetime";
+import { getDefaultIfUndefined } from "../../config";
 
 const genGPayCovidCardUrl = (
-  gpaySigner: { issuer: string; issuerId: string; privateKey: string },
+  gpaySigner: { issuer: string; issuerId: string },
   parsedFhirBundle: Bundle,
   uuid: string,
   storedUrl: string
 ) => {
-  const gpay = GPay(gpaySigner.privateKey);
+  const privateKey = getDefaultIfUndefined(
+    process.env.GPAY_COVID_CARD_PRIVATE_KEY,
+    ""
+  ).replace(/\\n/g, "\n");
+  const gpay = GPay(privateKey);
 
   const patientDetails: PatientDetails = {
     dateOfBirth: isoToDateOnlyString(parsedFhirBundle.patient.birthDate),
