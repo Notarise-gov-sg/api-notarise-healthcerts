486ede3ce008dce024a3a08ff0e4dce5f1661c0b
eugbyte <eugenetham1994@gmail.com>
2021-12-13 16:48:42 +0800

    chore: added error handling

--- src/models/fhir/maskNricFin.ts
+++ src/models/fhir/maskNricFin.ts
@@ -1,3 +1,4 @@
+import { getLogger } from "../../common/logger";
 import { HealthCertDocument } from "../../types";
 
 export interface NricIdentifier {
@@ -11,28 +12,40 @@ export interface NricIdentifier {
 // Get the reference to the nested nric identifier object
 // mask the value in place
 
+const { error: logError } = getLogger("maskNRIC");
+
 export const maskNRIC = (nric: string) => `${nric[0]}****${nric.slice(5)}`;
 
 export const getNRICIdentifierV1 = (
   data: HealthCertDocument
-): NricIdentifier => {
-  const entry = (data.fhirBundle.entry as any[]).find(
-    (ent) => ent.resourceType === "Patient"
-  );
-  const nricIdentifier = entry?.identifier?.find((ident: any) =>
-    ident.type?.text?.includes("NRIC")
-  );
-  return nricIdentifier;
+): NricIdentifier | null => {
+  try {
+    const entry = (data.fhirBundle.entry as any[]).find(
+      (ent) => ent.resourceType === "Patient"
+    );
+    const nricIdentifier = entry?.identifier?.find((ident: any) =>
+      ident.type?.text?.includes("NRIC")
+    );
+    return nricIdentifier;
+  } catch (error) {
+    logError(`cannot find nested nric identity object: ${error}`);
+    return null;
+  }
 };
 
 export const getNRICIdentifierV2 = (
   data: HealthCertDocument
-): NricIdentifier => {
-  const entry = (data.fhirBundle.entry as any[]).find(
-    (ent) => ent.resource?.resourceType === "Patient"
-  );
-  const nricIdentifier = entry?.resource?.identifier?.find((ident: any) =>
-    ident.id?.includes("NRIC")
-  );
-  return nricIdentifier;
+): NricIdentifier | null => {
+  try {
+    const entry = (data.fhirBundle.entry as any[]).find(
+      (ent) => ent.resource?.resourceType === "Patient"
+    );
+    const nricIdentifier = entry?.resource?.identifier?.find((ident: any) =>
+      ident.id?.includes("NRIC")
+    );
+    return nricIdentifier;
+  } catch (error) {
+    logError(`cannot find nested nric identity object: ${error}`);
+    return null;
+  }
 };
