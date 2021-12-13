8192ab5bbf91d04ae9ef83beecbadc9f9db6f512
eugbyte <eugenetham1994@gmail.com>
2021-12-13 16:33:41 +0800

    chore: remove tsconfig paths

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,4 @@ module.exports = {
     "\\.txt": "jest-raw-loader",
     "\\.subject": "jest-raw-loader",
   },
-
-  // A map from regular expressions to module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    "~/(.*)": "<rootDir>/src/$1",
-  },
 };
