8388bb8b37103ba21792cbf1d580c8f5dce21f78
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-12-31 10:44:34 +0800

    fix: lint errors

--- src/models/fhir/recognised.test.ts
+++ src/models/fhir/recognised.test.ts
@@ -7,7 +7,7 @@ import exampleMultiTypeHealthCertWithNric from "../../../test/fixtures/v2/pdt_pc
 
 const { PdtTypes } = pdtHealthCertV2;
 
-describe.only("recognised fields", () => {
+describe("recognised fields", () => {
   test("should throw error if single type HealthCert has an invalid test result code", () => {
     let thrownError;
     const parsedFhirBundle = fhirHelper.parse(
@@ -31,9 +31,7 @@ describe.only("recognised fields", () => {
     const parsedFhirBundle = fhirHelper.parse(
       exampleMultiTypeHealthCertWithNric.fhirBundle as R4.IBundle
     );
-    parsedFhirBundle.observations.forEach(({ observation }) => {
-      observation.result.code = "foobar";
-    });
+    parsedFhirBundle.observations[1].observation.result.code = "foobar";
     try {
       fhirHelper.hasRecognisedFields(PdtTypes.Art, parsedFhirBundle);
     } catch (e) {
@@ -42,7 +40,7 @@ describe.only("recognised fields", () => {
       }
     }
     expect(thrownError).toMatchInlineSnapshot(
-      `"Submitted HealthCert is invalid, the following fields in fhirBundle are not recognised: [\\"'0.Observation.valueCodeableConcept.coding[0].code' is an unrecognised code - please use of the following codes: 10828004,260385009,260373001,260415000\\",\\"'1.Observation.valueCodeableConcept.coding[0].code' is an unrecognised code - please use of the following codes: 10828004,260385009,260373001,260415000\\"]. For more info, refer to the mapping table here: https://github.com/Open-Attestation/schemata/pull/38"`
+      `"Submitted HealthCert is invalid, the following fields in fhirBundle are not recognised: [\\"'1.Observation.valueCodeableConcept.coding[0].code' is an unrecognised code - please use of the following codes: 10828004,260385009,260373001,260415000\\"]. For more info, refer to the mapping table here: https://github.com/Open-Attestation/schemata/pull/38"`
     );
   });
 });
