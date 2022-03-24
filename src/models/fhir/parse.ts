f110dce9b7714b0c1e9919b86176037b53bbbb8f
tngzixiang <tngzixiang@gmail.com>
2022-03-24 11:30:59 +0800

    Revert "Chore/181200750 art modality 2 (#500)"
    This reverts commit fe0c3ae4a8a1d1434fe7cd8a0ea01d9f31e0082b.

--- src/models/fhir/parse.ts
+++ src/models/fhir/parse.ts
@@ -75,7 +75,6 @@ export const parsers = (resource: R4.IResourceList | undefined) => {
         result: resource.valueCodeableConcept?.coding?.[0],
         effectiveDateTime: resource.effectiveDateTime,
         status: resource.status,
-        modality: resource.note?.find((n) => n.id === "MODALITY")?.text,
       } as ParsedObservation;
 
     case "Practitioner":
