8849f868240aa01e3588791cd9e389f5d074c370
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2022-04-27 14:40:25 +0800

    fix: check vault salt log

--- src/common/hash.ts
+++ src/common/hash.ts
@@ -1,7 +1,11 @@
 import { createHmac } from "crypto";
 import { config } from "../config";
+import { getLogger } from "./logger";
+
+const { trace } = getLogger("src/common/hash");
 
 const saltAndHash = (id: string) => {
+  trace("vaultUinSalt : ", config.vaultUinSalt);
   const hasher = createHmac("sha256", config.vaultUinSalt);
   return hasher.update(id).digest("hex");
 };
