fc741f19f9798b9b22091b63354f12897c053c2c
Lucas <31716292+yapyuyou@users.noreply.github.com>
2022-07-06 13:59:22 +0800

    fix: test

--- src/services/vault/index.test.ts
+++ src/services/vault/index.test.ts
@@ -1,27 +1,15 @@
-import axios from "axios";
-import { getPersonalDataFromVault, checkValidPatientName } from "./index";
+// import axios from "axios";
+// import { getPersonalDataFromVault, checkValidPatientName } from "./index";
+import { checkValidPatientName } from "./index";
 
-const mockData: any = {
-  uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
-  dateofbirth: "1990-01-15",
-  gender: "F",
-  principalname: "Tan Chen Chen",
-};
+// const mockData: any = {
+//   uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
+//   dateofbirth: "1990-01-15",
+//   gender: "F",
+//   principalname: "Tan Chen Chen",
+// };
 
 describe("test vaultService", () => {
-  // it("should return vault data for valid uin", async () => {
-  //   const mockFIN = "S3001470G";
-  //   jest.spyOn(axios, "get").mockResolvedValue(mockData);
-  //   await expect(
-  //     getPersonalDataFromVault(mockFIN, "abc")
-  //   ).resolves.toMatchObject({
-  //     uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
-  //     dateofbirth: "1990-01-15",
-  //     gender: "F",
-  //     principalname: "Tan Chen Chen",
-  //   });
-  // });
-
   it("check the patient name if it's contain in principal name", async () => {
     expect(checkValidPatientName("Tan Chen Chen", "Tan Chen Chen")).toBe(true);
     expect(checkValidPatientName("Tan Chen", "Tan Chen Chen")).toBe(true);
