aba1c8a5cbcbaa188b8b653cd6ab22cacd15e3f4
Lucas <31716292+yapyuyou@users.noreply.github.com>
2022-07-08 13:28:35 +0800

    fix: test

--- src/services/vault/index.test.ts
+++ src/services/vault/index.test.ts
@@ -1,15 +1,58 @@
-// import axios from "axios";
-// import { getPersonalDataFromVault, checkValidPatientName } from "./index";
-import { checkValidPatientName } from "./index";
+import axios from "axios";
+import { getDemographics, checkValidPatientName } from "./index";
 
-// const mockData: any = {
-//   uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
-//   dateofbirth: "1990-01-15",
-//   gender: "F",
-//   principalname: "Tan Chen Chen",
-// };
+const mockData: any = {
+  data: {
+    vaultData: [
+      {
+        uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
+        dateofbirth: "1990-01-15",
+        gender: "F",
+        principalname: "Tan Chen Chen",
+      },
+    ],
+    manualData: [
+      {
+        uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
+        dateofbirth: "1990-01-15",
+        gender: "M",
+        principalname: "Tan Chen Chen",
+      },
+    ],
+  },
+};
+
+const expectedData: any = {
+  vaultData: [
+    {
+      uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
+      dateofbirth: "1990-01-15",
+      gender: "F",
+      principalname: "Tan Chen Chen",
+    },
+  ],
+  manualData: [
+    {
+      uin: "e44f2ee89027d641659f23f9d6188d16cc576bcfd57d21caa032b50e75758657",
+      dateofbirth: "1990-01-15",
+      gender: "M",
+      principalname: "Tan Chen Chen",
+    },
+  ],
+};
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("test vaultService", () => {
+  it("should return vault data for valid uin", async () => {
+    const mockFIN = "S3001470G";
+    mockedAxios.get.mockResolvedValue(mockData);
+    await expect(getDemographics(mockFIN, "abc")).resolves.toMatchObject(
+      expectedData
+    );
+  });
+
   it("check the patient name if it's contain in principal name", async () => {
     expect(checkValidPatientName("Tan Chen Chen", "Tan Chen Chen")).toBe(true);
     expect(checkValidPatientName("Tan Chen", "Tan Chen Chen")).toBe(true);
