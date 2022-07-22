54ed222f239d21cb9da07aad1aa1cdbb123627c9
Lucas <31716292+yapyuyou@users.noreply.github.com>
2022-07-22 16:32:31 +0800

    chore: add merge script

---
+++ scripts/mergeProviderJsons.ts
@@ -0,0 +1,59 @@
+import fse from "fs-extra";
+
+type mergedEntry = {
+  name: string;
+  id: string;
+  domain?: string;
+};
+
+type apiKeyIdEntry = {
+  id: string;
+  name: string;
+};
+
+type domainEntry = {
+  domain: string;
+  name: string;
+};
+
+const params = process.argv.slice(2);
+if (params.length < 2) {
+  throw new Error("Provider api key id path required");
+}
+
+const apiKeyIdsPath = params[0];
+const apiKeyIdsEntries = fse.readFileSync(apiKeyIdsPath).toString();
+const apiKeyIds: apiKeyIdEntry[] = JSON.parse(apiKeyIdsEntries);
+
+const domainsPath = params[1];
+const domainsEntries = fse.readFileSync(domainsPath).toString();
+const domains: domainEntry[] = JSON.parse(domainsEntries);
+
+const merged: mergedEntry[] = [];
+const domainsDict: { [key: string]: string[] } = {};
+domains.forEach((entry) => {
+  if (domainsDict[entry.name] != undefined) {
+    domainsDict[entry.name].push(entry.domain);
+  } else {
+    domainsDict[entry.name] = [entry.domain];
+  }
+});
+
+apiKeyIds.forEach((entry) => {
+  if (domainsDict[entry.name] != undefined) {
+    domainsDict[entry.name].forEach((domain) => {
+      merged.push({ ...entry, domain });
+    });
+  } else {
+    merged.push(entry);
+  }
+});
+
+merged.sort(function (a, b) {
+  return a.hasOwnProperty("domain") ? -1 : b.hasOwnProperty("domain") ? 1 : 0;
+});
+
+fse.outputFileSync(
+  "src/static/provider_apikeyid.merged.json",
+  JSON.stringify(merged, null, 2)
+);
