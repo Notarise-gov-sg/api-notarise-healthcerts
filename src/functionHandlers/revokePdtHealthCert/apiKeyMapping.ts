c8bb956d60f865e4b3bfe21559f4d69961c4f517
Tng Zi Xiang <54768054+zixiang2018@users.noreply.github.com>
2022-08-02 11:30:42 +0800

    chore: clean up

--- src/functionHandlers/revokePdtHealthCert/apiKeyMapping.ts
+++ src/functionHandlers/revokePdtHealthCert/apiKeyMapping.ts
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import providerApiKeyMapping from "../../static/provider_apikeyid.merged.json";
 
-// Get provider domain given API key
 export const getProvider = (providerApiKey: any) =>
   _.find(providerApiKeyMapping, { id: providerApiKey });
